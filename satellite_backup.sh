0d7103962ce276935bf3f4794d046b3be815028d
Talor Holloway <thollowa@redhat.com>
2018-03-19 13:09:42 +1100

    fixed typo

--- satellite_backup.sh
+++ satellite_backup.sh
@@ -291,7 +291,7 @@ run_expiration()
 	done
 
 	msg Removing Weekly Full Backups - ${RETENTION} days plus 7 to cater for a weeks worth of incrementals tied to the full
-	for full_dir in $(/usr/bin/find ${BACKUP_DIRECTORY}/full -type d -mtime +$(( $RETENTION + 7)) -name "*katello-backup*" -o type d -mtime +$(( $RETENTION + 7)) -name "*satellite-backup*")
+	for full_dir in $(/usr/bin/find ${BACKUP_DIRECTORY}/full -type d -mtime +$(( $RETENTION + 7)) -name "*katello-backup*" -o -type d -mtime +$(( $RETENTION + 7)) -name "*satellite-backup*")
 	do
 		msg70 Removing $full_dir
 		rm -rf $full_dir
